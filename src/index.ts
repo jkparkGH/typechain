63b35666957395c336d91268473c2c28a339335c
jake <pjklogs@naver.com>
2020-10-16 13:42:10 +0900

    - typescript interface 기본 && 함수 선언

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-const nameString = "jake",
+interface Human {
+  humanName: string;
+  age: number;
+  gender: string;
+}
+
+const humanName = "jake",
   age = 30,
   gender = "male";
 
-const sayHello = (nameString: string, age: number, gender: string): void => {
+const jake = {
+  humanName: "jake",
+  age: 30,
+  gender: "male"
+};
+
+const sayHello = (humanName: string, age: number, gender: string): void => {
   console.log(
-    `HI, My name is ${nameString}, I ${age} years old and I'm ${gender}`
+    `### sayHello 1 ::  HI, My name is ${humanName}, I ${age} years old and I'm ${gender}`
+  );
+};
+const sayHello2 = (person: Human): void => {
+  console.log(
+    `### sayHello 3 ::  HI, My name is ${person.humanName}, I ${person.age} years old and I'm ${person.gender}`
+  );
+};
+const sayHello3 = ({ humanName, age, gender }: Human): void => {
+  console.log(
+    `### sayHello 2 ::  HI, My name is ${humanName}, I ${age} years old and I'm ${gender}`
   );
 };
 
-sayHello(nameString, age, gender);
+sayHello(jake.humanName, jake.age, jake.gender);
+sayHello2(jake);
+sayHello3(jake);
