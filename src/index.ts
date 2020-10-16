e525c6323277e63c6839ed3ed276f5381dd1efbe
jake <pjklogs@naver.com>
2020-10-16 14:09:35 +0900

    - ts class 찍먹

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,54 @@
 interface Human {
-  humanName: string;
+  name: string;
   age: number;
   gender: string;
 }
 
-const humanName = "jake",
-  age = 30,
-  gender = "male";
+class Person {
+  public name: string;
+  public age: number;
+  public gender: string;
+  private weight: number | string;
+  constructor(
+    name: string,
+    age: number,
+    gender: string,
+    weight: number | string = "00"
+  ) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.weight = weight;
+  }
+  getPersonWeight = () => {
+    return `${this.weight}kg`;
+  };
+}
 
 const jake = {
-  humanName: "jake",
+  name: "jake",
   age: 30,
   gender: "male"
 };
 
-const sayHello = (humanName: string, age: number, gender: string): void => {
+const lyn = new Person("lyn", 29, "male", 68);
+
+const sayHello = (name: string, age: number, gender: string): void => {
   console.log(
-    `### sayHello 1 ::  HI, My name is ${humanName}, I ${age} years old and I'm ${gender}`
+    `### sayHello 1 ::  HI, My name is ${name}, I ${age} years old and I'm ${gender}`
   );
 };
 const sayHello2 = (person: Human): void => {
   console.log(
-    `### sayHello 3 ::  HI, My name is ${person.humanName}, I ${person.age} years old and I'm ${person.gender}`
+    `### sayHello 3 ::  HI, My name is ${person.name}, I ${person.age} years old and I'm ${person.gender}`
   );
 };
-const sayHello3 = ({ humanName, age, gender }: Human): void => {
+const sayHello3 = ({ name, age, gender, getPersonWeight }: Person): void => {
   console.log(
-    `### sayHello 2 ::  HI, My name is ${humanName}, I ${age} years old and I'm ${gender}`
+    `### sayHello 2 ::  HI, My name is ${name}, I ${age} years old and I'm ${gender}, my weight ${getPersonWeight()}`
   );
 };
 
-sayHello(jake.humanName, jake.age, jake.gender);
+sayHello(jake.name, jake.age, jake.gender);
 sayHello2(jake);
-sayHello3(jake);
+sayHello3(lyn);
