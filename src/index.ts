d41f8c4ae573357b0b660a315360dc6a48ba7cba
jake <pjklogs@naver.com>
2020-10-16 13:31:35 +0900

    - add tsc-watch - add type to function parmeters

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ const nameString = "jake",
   age = 30,
   gender = "male";
 
-const sayHello = (nameString, age, gender?) => {
+const sayHello = (nameString: string, age: number, gender: string): void => {
   console.log(
     `HI, My name is ${nameString}, I ${age} years old and I'm ${gender}`
   );
 };
 
-sayHello(nameString, age);
+sayHello(nameString, age, gender);
